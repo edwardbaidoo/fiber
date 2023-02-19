a5ca0dc9113359124e340fa5ed579d4433fa3452
Edward Baidoo <edwardjacobbaidoo@gmail.com>
2023-02-19 23:10:44 +0000

    There are some errors, But this is an early work on the Login route

--- controllers/auth/auth.go
+++ controllers/auth/auth.go
@@ -6,10 +6,13 @@ import (
 	"fiber/database"
 	"fiber/model"
 	"fiber/utils"
+	"fmt"
 	"log"
 	"time"
 
 	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type SignupRequest struct {
@@ -18,12 +21,21 @@ type SignupRequest struct {
 	Phone    string
 }
 
+type LoginRequest struct {
+	Email    string
+	Password string
+}
+
 func CreateUser(c *fiber.Ctx) error {
+	/*
+		Steps
+		1. Conned
+	*/
 	var DB = database.ConnectDatabase()
 	// usersCollection := collections.GetUsersCollections(DB, os.Getenv("UsersCollectionName"))
 	usersCollection := collections.GetUsersCollections(DB, "Users")
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	user := new(model.User)
+	userSignupRequestData := new(model.User)
 	defer cancel()
 
 	req := new(SignupRequest)
@@ -36,7 +48,7 @@ func CreateUser(c *fiber.Ctx) error {
 		})
 		return err
 	}
-	log.Println(req)
+	//log.Println(req)
 
 	if req.Email == "" || req.Password == "" || req.Phone == "" {
 		return fiber.NewError(fiber.StatusBadRequest, "Invalid Signup Data")
@@ -47,14 +59,14 @@ func CreateUser(c *fiber.Ctx) error {
 		log.Fatal(err)
 	}
 
-	user = &model.User{
+	userSignupRequestData = &model.User{
 		Email:     req.Email,
 		Password:  salltedPassword,
 		Phone:     req.Phone,
 		CreatedAt: time.Now()}
 
-	log.Println(user)
-	result, err := usersCollection.InsertOne(ctx, user)
+	// log.Println(userSignupRequestData)
+	result, err := usersCollection.InsertOne(ctx, userSignupRequestData)
 	if err != nil {
 		c.Status(400).JSON(&fiber.Map{
 			"success": false,
@@ -71,6 +83,28 @@ func CreateUser(c *fiber.Ctx) error {
 	return nil
 }
 
-func login() {
+func Login(c *fiber.Ctx) error {
+	req := new(LoginRequest)
+	err := c.BodyParser(req)
+	if err != nil {
+		c.Status(400).JSON(&fiber.Map{
+			"success": false,
+			"message": err,
+			"data":    nil,
+		})
+		return err
+	}
+	var DB = database.ConnectDatabase()
+	usersCollection := collections.GetUsersCollections(DB, "Users")
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
 
+	var response primitive.M
+
+	user, err := usersCollection.FindOne(ctx, bson.D{{"email", req.Email}}).Decode(&response)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	return nil
 }
--- model/userModel.go
+++ model/userModel.go
@@ -7,4 +7,5 @@ type User struct {
 	Password  string    `json:"_"`
 	Phone     string    `json:"phone"`
 	CreatedAt time.Time `json:"created_at"`
+	Token     string
 }
